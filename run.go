1dbcc22d254ed1581bd5395aa68e02dee37b9050
aks <aks@skycoin.net>
2014-03-01 00:04:04 -0800

    updated instructions

--- run.go
+++ run.go
@@ -3,10 +3,15 @@ package main
 import (
 	//"github.com/skycoin/darknet/src/lib/secp256k1-go"
 	//"github.com/skycoin/gnet"
+	//"github.com/skycoin/encoder"
 )
 
 //read gnet godoc, get two clients connecting to each other
 // https://godoc.org/github.com/skycoin/gnet
+
+//read encoder godoc. encoder is for serializing structs to
+// bytes for transport over network
+// https://godoc.org/github.com/skycoin/encoder
 
 func main() {
 
