17c9dfdce91f43d2c0a42351a2fe025c9d63d3a7
aks <aks@skycoin.net>
2014-02-28 23:58:58 -0800

    darknet

---
+++ run.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	//"github.com/skycoin/darknet/src/lib/secp256k1-go"
+	//"github.com/skycoin/gnet"
+)
+
+//read gnet godoc, get two clients connecting to each other
+// https://godoc.org/github.com/skycoin/gnet
+
+func main() {
+
+
+
+}
